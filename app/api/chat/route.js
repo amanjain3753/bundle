66952f02cc322ca3ab220ebaa2438a395ede9311
amanjain3753 <amanjain3753@gmail.com>
2026-03-05 17:17:07 +0530

    added

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -2,7 +2,7 @@ export async function POST(req) {
   const body = await req.json();
 
   const res = await fetch(
-    "http://localhost:5678/webhook/48b4f655-28b1-4deb-bd09-1fb7e1fe2792",
+    " https://subtegumental-eladia-proctodaeal.ngrok-free.dev/webhook/48b4f655-28b1-4deb-bd09-1fb7e1fe2792",
     {
       method: "POST",
       headers: {
@@ -18,3 +18,7 @@ export async function POST(req) {
     headers: { "Content-Type": "application/json" }
   });
 }
+
+
+
+// 3AWajef8bzRFB25YB3Daa0RvyoE_21uCkak8XvRpL9uaLaRnC  toke of ngork
